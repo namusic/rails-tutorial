18c2cccde62d9e868a51ac6e047052db5410b57c
na_bot_o <namusic7010@gmail.com>
2018-09-11 12:34:41 +0100

    implement basic login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -12,6 +12,11 @@ module SessionsHelper
   end
   
   def logged_in?
-    !currenct_user.nil?
+    !current_user.nil?
+  end
+
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
   end
 end
