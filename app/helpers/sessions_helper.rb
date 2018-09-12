60c29c139274a74272df957beb670d2260bd7168
na_bot_o <namusic7010@gmail.com>
2018-09-12 11:02:22 +0100

    add forget session and cookies when logout

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -12,8 +12,8 @@ module SessionsHelper
 
 
   def current_user
-    if (user_id = session[:user_id]
-      @current_user ||= User.find_by(id: :user_id)
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
     elsif (user_id = cookies.signed[:user_id])
       user = User.find_by(id: user_id)
       if user && user.authenticated?(cookies[:remember_token])
@@ -27,7 +27,14 @@ module SessionsHelper
     !current_user.nil?
   end
 
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
