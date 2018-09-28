edf1caaa986df018b33ccf1b7fa5f1825b4b5f69
na_bot_o <namusic7010@gmail.com>
2018-09-28 05:06:07 +0100

    add  following and followed method

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,12 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to:'sessions#destroy'
+
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
   resources :users
